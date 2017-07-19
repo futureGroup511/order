480666b7d47363700aaf11a7e8035a5d3494c599
1763608200 <1763608200@qq.com>
2017-07-19 10:41:28 +0800

    超时提示

--- src/main/java/com/future/order/filter/LoginFilter.java
+++ src/main/java/com/future/order/filter/LoginFilter.java
@@ -1,6 +1,9 @@
 package com.future.order.filter;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,7 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.future.order.entity.User;
 
@@ -49,23 +52,15 @@ public class LoginFilter implements Filter {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8"); // 转码
 		HttpServletRequest hRequest = (HttpServletRequest) request;
-		HttpServletResponse hResponse = (HttpServletResponse) response;
 		User user = (User) hRequest.getSession().getAttribute("user");// 获得登陆用户
-		String path = hRequest.getRequestURI();
 		String returnUrl = hRequest.getContextPath() + "/index.jsp";
-		if(hRequest.getSession(false)!=null){
-			response.getWriter()
-			.println("<script language=\"javascript\">" +"alert(\"登录超时！请重新登录\");"+ "if(window.opener==null){window.top.location.href=\""
-					+ returnUrl + "\";}else{window.opener.top.location.href=\"" + returnUrl
-					+ "\";window.close();}</script>");
-			return;	
-		}
+		
 		if (user != null) {
 			chain.doFilter(request, response);
 			return;
 		} else {
 			response.getWriter()
-					.println("<script language=\"javascript\">" + "if(window.opener==null){window.top.location.href=\""
+					.println("<script language=\"javascript\">" +"alert(\"登录超时！请重新登录\");"+ "if(window.opener==null){window.top.location.href=\""
 							+ returnUrl + "\";}else{window.opener.top.location.href=\"" + returnUrl
 							+ "\";window.close();}</script>");
 			return;
