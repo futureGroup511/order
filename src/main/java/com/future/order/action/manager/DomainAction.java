2f57725c6470567257d83b168fae8aa7d1e251e5
maliming123 <1365413148@qq.com>
2017-07-22 21:42:17 +0800

    ip

---
+++ src/main/java/com/future/order/action/manager/DomainAction.java
@@ -0,0 +1,7 @@
+package com.future.order.action.manager;
+
+import com.future.order.base.BaseAction;
+
+public class DomainAction extends BaseAction{
+
+}
--- src/main/java/com/future/order/action/manager/TableManagerAction.java
+++ src/main/java/com/future/order/action/manager/TableManagerAction.java
@@ -216,37 +216,6 @@ private static String getInternetIp(){
         throw new RuntimeException(e);
     }
 }
-	public static String getLocalIP() {
-		  String sIP = "";
-		  InetAddress ip = null;
-		  try {
-		   boolean bFindIP = false;
-		   Enumeration<NetworkInterface> netInterfaces = (Enumeration<NetworkInterface>) NetworkInterface
-		     .getNetworkInterfaces();
-		   while (netInterfaces.hasMoreElements()) {
-		    if (bFindIP) {
-		     break;
-		    }
-		    NetworkInterface ni = (NetworkInterface) netInterfaces
-		      .nextElement();
-		    Enumeration<InetAddress> ips = ni.getInetAddresses();
-		    while (ips.hasMoreElements()) {
-		     ip = (InetAddress) ips.nextElement();
-		     if (!ip.isLoopbackAddress() 
-		       && ip.getHostAddress().matches(
-		         "(\\d{1,3}\\.){3}\\d{1,3}")) {
-		      bFindIP = true;
-		      break;
-		     }
-		    }
-		   }
-		  } catch (Exception e) {
-		  }
-		  if (null != ip) {
-		   sIP = ip.getHostAddress();
-		  }
-		  return sIP;
-		 }
 	public Tables getTable() {
 		return table;
 	}
