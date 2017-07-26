16117a7912c62f74408ca99aba78cb3e91a3bd7e
1763608200 <1763608200@qq.com>
2017-07-26 21:19:24 +0800

    收银员优化

--- src/main/java/com/future/order/action/manager/StockDetailsAction.java
+++ src/main/java/com/future/order/action/manager/StockDetailsAction.java
@@ -1,7 +1,20 @@
 package com.future.order.action.manager;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.future.order.base.BaseAction;
 import com.future.order.entity.Ingredient;
@@ -16,6 +29,21 @@ public class StockDetailsAction extends BaseAction{
 	private StockDetails details;
 	private String ask;
 	private String inquiry;
+	
+	private File myFileName;
+	
+	/**
+	 * @return the myFileName
+	 */
+	public File getMyFileName() {
+		return myFileName;
+	}
+	/**
+	 * @param myFileName the myFileName to set
+	 */
+	public void setMyFileName(File myFileName) {
+		this.myFileName = myFileName;
+	}
 	public String CheckStockDetails(){
 		session.put("stockid", id);
 		return  this.execute();	
@@ -130,7 +158,34 @@ public class StockDetailsAction extends BaseAction{
 		session.put("inquiry", inquiry);	
 		return "details";
 	}
+	public void uploadImg() throws Exception{
+
+
+		HttpServletRequest req2 = ServletActionContext.getRequest();
+		 HttpServletResponse res2 = ServletActionContext.getResponse(); 
+	       String name=myFileName.getName();
+	        // 提取文件拓展名
+	        String fileNameExtension = name.substring(name.indexOf("."), name.length() - 1);
+	        // 生成实际存储的真实文件名
+	        String realName = UUID.randomUUID().toString() + fileNameExtension;
+	        // 图片存放的真实路径
+	        String realPath = req2.getServletContext().getRealPath("/uploadImg/source") + "/" + realName;
+	        // 将文件写入指定路径下
+	        InputStream in = new FileInputStream(myFileName);
+	        File uploadFile = new File(realPath);
+			OutputStream out = new FileOutputStream(uploadFile);
+			byte[] buffer = new byte[1024 * 1024];
+			int length;
+			while ((length = in.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+			in.close();
+			out.close();
+	        
+	        // 返回图片的URL地址
+	        res2.getWriter().write(req2.getContextPath() + "/uploadImg/source/" + realName);
 	
+	}
 	public int getId() {
 		return id;
 	}
